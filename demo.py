530a9cea2f413301a19d5c58130522fa9393e6f5
Dániel Vince <vinced@inf.u-szeged.hu>
2023-02-06 16:02:06 +0100

    Update Demo script

--- demo.py
+++ demo.py
@@ -6,8 +6,8 @@
 # according to those terms.
 
 import sys
-from os import environ
-from os.path import abspath, basename, dirname, join, splitext
+from os import environ, makedirs
+from os.path import abspath, dirname, exists, join
 
 ROOT_DIR = dirname(abspath(__file__))
 TOOLS_DIR = join(ROOT_DIR, 'tools')
@@ -16,7 +16,7 @@ sys.path.append(TOOLS_DIR)
 from build_jerry import checkout_and_build
 from command_runner import run_command
 from common import GRAMMARS, TEST_FILES, dump_json, process_source
-
+from generate_oracle import generate_oracle
 
 def process_log(log_path):
     with open(log_path, 'r') as log:
@@ -27,8 +27,8 @@ def process_log(log_path):
     cache_count = src.count("cache = 'PASS'")
 
     for line in src.splitlines():
-        if line.startswith('Result is saved to '):
-            result_file_path = line.replace('Result is saved to ', '').strip('.')
+        if line.startswith('Output saved to '):
+            result_file_path = line.replace('Output saved to ', '').strip('.')
             output_lines, output_chars, output_size = process_source(result_file_path)
             return test_fail, test_pass, cache_count, output_lines, output_chars
     assert False, 'Result save line not found.'
@@ -38,24 +38,34 @@ if __name__ == '__main__':
   grammar, start_rule = GRAMMARS['js']
   report = dict()
 
-  for test_name, (oracle, input_file) in TEST_FILES.items():
-    checkout_and_build(test_name)
-
+  for test_name, (input_file, message) in TEST_FILES.items():
     test_dir = join(ROOT_DIR, 'tests', test_name)
     input_source = join(test_dir, input_file)
+    executable = join(test_dir, f'{test_name}-jerry')
+    oracle = join(test_dir, 'test.sh')
+
+    if not exists(executable):
+      checkout_and_build(test_name, target=executable)
+
+    if not exists(oracle):
+      generate_oracle(test_name, executable, message, oracle)
+
+
+    out_dir = join(ROOT_DIR, 'experiments', test_name)
+    makedirs(out_dir, exist_ok=True)
 
     command = [
         'picireny',
         '--log-level', 'DEBUG',
         '--start', start_rule,
-        '--test', join(test_dir, oracle),
+        '--test', oracle,
         '--input', input_source,
-        '--out', join(ROOT_DIR, 'experiments', test_name),
+        '--out', out_dir,
         '--grammar',
     ]
     command.extend(grammar)
 
-    log_file = join(test_dir, 'reduction.log')
+    log_file = join(out_dir, 'reduction.log')
     with open(log_file, 'w') as stderr_file:
         run_command(command, test_dir, out=stderr_file, env=dict(environ, PYTHONOPTIMIZE='1'))
 
